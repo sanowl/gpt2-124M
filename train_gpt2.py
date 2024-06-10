7de664021fd3a65dc82b4e86bd446f93c3be7aaa
San <99511815+sanowl@users.noreply.github.com>
2024-06-10 15:32:08 +0300

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -15,3 +15,12 @@ class GPTk(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
+        
+        self.transformer = nn.modules(dict(
+          wte =nn.Embedding(config.vocab_size, config.n_emdb),
+          wpe =nn.Embedding(config.block_size,  config.embd),
+          h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+          ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size ,bias= False)
+        
